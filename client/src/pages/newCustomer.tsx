65aa81aa23d1afe7dcc1ba451a5359ede7f135ee
Ting <55216128+f27dxx@users.noreply.github.com>
2022-04-08 16:47:47 +1000

    create more input fields to match database schema

--- client/src/pages/newCustomer.tsx
+++ client/src/pages/newCustomer.tsx
@@ -1,20 +1,10 @@
-import {
-  Box,
-  Button,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Heading,
-  HStack,
-  Input,
-  VStack,
-} from "@chakra-ui/react"
-import { Field, Formik, useFormik } from "formik"
+import { Box, Button, Heading } from "@chakra-ui/react"
+import { Formik } from "formik"
 import * as Yup from "yup"
 import InputField from "../components/InputField"
 
 interface Props {}
-const NewCustomer: React.FC<{}> = ({}) => {
+const NewCustomer: React.FC<Props> = () => {
   const now = new Date()
   return (
     <Formik
@@ -22,8 +12,17 @@ const NewCustomer: React.FC<{}> = ({}) => {
         firstName: "",
         lastName: "",
         createdAt: now.toString(),
+        DOB: "",
         email: "",
         phone: "",
+        address: {
+          country: "Australia",
+          line1: "",
+          line2: "",
+          postcode: "",
+          city: "",
+          state: "",
+        },
       }}
       validationSchema={Yup.object({
         firstName: Yup.string().required("First name required"),
@@ -31,6 +30,16 @@ const NewCustomer: React.FC<{}> = ({}) => {
         createdAt: Yup.string().required("created at required"),
         email: Yup.string().required("Email required").email("Invalid Email"),
         phone: Yup.string().required("Phone Number required"),
+        DOB: Yup.string().required("DOB required"),
+
+        address: Yup.object({
+          country: Yup.string().required("country is required"),
+          line1: Yup.string().required("line1 is required"),
+          line2: Yup.string().required("line2 is required"),
+          postcode: Yup.string().required("postcode is required"),
+          city: Yup.string().required("city is required"),
+          state: Yup.string().required("state is required"),
+        }),
       })}
       onSubmit={(values, actions) => {
         console.log(values)
@@ -48,35 +57,73 @@ const NewCustomer: React.FC<{}> = ({}) => {
           </Box>
 
           <form onSubmit={formik.handleSubmit}>
-            <VStack>
-              <HStack w="100%">
-                <InputField
-                  name="firstName"
-                  type="text"
-                  label="First Name"
-                  placeholder="Harry"
-                />
-                <InputField
-                  name="lastName"
-                  type="text"
-                  label="Last Name"
-                  placeholder="Potter"
-                />
-              </HStack>
+            <InputField
+              name="firstName"
+              type="text"
+              label="First Name"
+              placeholder="Harry"
+            />
+            <InputField
+              name="lastName"
+              type="text"
+              label="Last Name"
+              placeholder="Potter"
+            />
 
-              <InputField
-                name="email"
-                type="email"
-                label="Email"
-                placeholder="customer@domain.com"
-              />
-              <InputField
-                name="phone"
-                type="text"
-                label="Phone No."
-                placeholder="+61 ..."
-              />
-            </VStack>
+            <InputField
+              name="email"
+              type="email"
+              label="Email"
+              placeholder="customer@domain.com"
+            />
+            <InputField
+              name="phone"
+              type="text"
+              label="Phone No."
+              placeholder="+61 ..."
+            />
+            <InputField
+              name="DOB"
+              type="text"
+              label="Date of Birth"
+              placeholder="DD/MM/YYYY"
+            />
+            <InputField
+              name="address.country"
+              type="text"
+              label="Country"
+              placeholder="Australia"
+            />
+            <InputField
+              name="address.line1"
+              type="text"
+              label="Line1"
+              placeholder="line 1"
+            />
+            <InputField
+              name="address.line2"
+              type="text"
+              label="Line2"
+              placeholder="line2"
+            />
+            <InputField
+              name="address.postcode"
+              type="text"
+              label="Postcode"
+              placeholder="Postcode"
+            />
+            <InputField
+              name="address.city"
+              type="text"
+              label="City"
+              placeholder="Melbourne"
+            />
+            <InputField
+              name="address.state"
+              type="text"
+              label="State"
+              placeholder="VIC"
+            />
             <Button type="submit" colorScheme="blue" w="100%">
               Submit
             </Button>
