303cbed6a30fbc1eda4a80ce8844cf271e7a9015
Ting <55216128+f27dxx@users.noreply.github.com>
2022-04-27 10:26:20 +1000

    initial commit

--- client/src/pages/singleCustomer.tsx
+++ client/src/pages/singleCustomer.tsx
@@ -1,36 +1,38 @@
-import { Box, Center, Heading } from "@chakra-ui/react"
+import { Box, Heading, SimpleGrid } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
-import { useNavigate, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
+import CustomerDetailsSection from "../components/CustomerDetailsSection"
 import CustomerDAO from "../DAOs/CustomerDAO"
 import { Customer } from "../types/types"
+import NotFound from "./notFound"
+import TodoPage from "./todo"
 
-const SingleCustomer = () => {
+interface Props {}
+
+const SingleCustomer: React.FC<Props> = () => {
   const { customerId } = useParams() as { customerId: string }
   const [customer, setCustomer] = useState<Customer | null>(null)
-  const navigate = useNavigate()
 
   useEffect(() => {
     const getSingleCustomer = async (customerId: string) => {
-      console.log(customerId, "customerId")
       const customerFromServer = await CustomerDAO.selectId(customerId)
       if (customerFromServer) {
         setCustomer(customerFromServer)
-      } else {
-        navigate("/404")
       }
     }
     getSingleCustomer(customerId)
   }, [])
 
   return (
-    <Box bg="gray.50" px={[4, 4, 20, 20]} h="100vh">
-      <Center h="100%">
-        {customer ? (
-          <Heading size="4xl">{customer.firstName}</Heading>
-        ) : (
-          <p>Loading ... </p>
-        )}
-      </Center>
+    <Box px={[4, 4, 20, 20]} h="100vh">
+      {customer ? (
+        <SimpleGrid columns={[1, 1, 2, 2]}>
+          <CustomerDetailsSection customer={customer} />
+          <TodoPage />
+        </SimpleGrid>
+      ) : (
+        <NotFound />
+      )}
     </Box>
   )
 }
